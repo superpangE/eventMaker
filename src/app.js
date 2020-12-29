3856469a087e5a0bbe3e349354be0d2632e37a00
DESKTOP-J5O26G9\admin <glesmallow@gmail.com>
2020-12-29 21:47:55 +0900

    [feat] dotenv, API 작성

--- src/app.js
+++ src/app.js
@@ -1,19 +1,14 @@
 const express = require('express');
 const path = require('path');
-const mysql = require('mysql');
+const connection = require('./databases/db_starter');
+const columnRouter = require('./routes/column');
+
+require('dotenv').config();
 
 const app = express();
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-var connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: 'alsckddl1!',
-  database: 'eventMaker',
-});
-
 connection.connect();
 
-app.listen(3000);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/column', columnRouter);
+
+app.listen(process.env.PORT);
