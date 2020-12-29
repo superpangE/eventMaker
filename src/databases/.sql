58514d51ca6c05e1134ac092649183874bfb5a17
DESKTOP-J5O26G9\admin <glesmallow@gmail.com>
2020-12-29 21:47:33 +0900

    [feat] DB 연동 코드를 databases 폴더로 분리해서 저장

---
+++ src/databases/.sql
@@ -0,0 +1,18 @@
+create table eventMaker.Column(
+    column_id Integer PRIMARY KEY,
+    title char(30) not null,
+    pos Integer not null,
+    create_At DATETIME DEFAULT NOW()
+);
+
+create table eventMaker.Card(
+  card_id Integer not null,
+  column_id Integer not null,
+  title Integer not null,
+  pos Integer not null,
+  content text not null,
+  author char(30) not null,
+  create_At DATETIME DEFAULT NOW(),
+  PRIMARY KEY(card_id),
+  FOREIGN KEY(column_id) REFERENCES eventMaker.Column(column_id)
+);
---
+++ src/databases/db_starter.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql');
+
+require('dotenv').config();
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PW,
+  database: process.env.DB_DATABASE,
+});
+
+module.exports = connection;
