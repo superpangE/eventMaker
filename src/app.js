3b4bcae0dd3a4f4a87ae468a1870c0213ff3d21a
DESKTOP-J5O26G9\admin <glesmallow@gmail.com>
2021-01-11 22:05:21 +0900

    [refactor] JSON 형식으로 request에 body 속성을 추가
    POST 방식으로 받을 때 body에 저장된 값들을 가져오기 위해 속성 추가

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const connection = require('./databases/db_starter');
 const columnRouter = require('./routes/column');
+const cardRouter = require('./routes/card');
 
 require('dotenv').config();
 
@@ -9,6 +10,10 @@ const app = express();
 connection.connect();
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use('/column', columnRouter);
+app.use('/card', cardRouter);
 
 app.listen(process.env.PORT);
