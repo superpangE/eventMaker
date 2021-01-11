ddc350f10c5921d7e7b25c6c695fd9420e395cc8
DESKTOP-J5O26G9\admin <glesmallow@gmail.com>
2021-01-11 22:04:23 +0900

    [feat] 컬럼 생성, 수정, 삭제 API 작성

--- src/routes/column.js
+++ src/routes/column.js
@@ -4,13 +4,60 @@ const connection = require('../databases/db_starter');
 const router = express.Router();
 
 router.get('/findAll', (req, res) => {
-  const sql = 'SELECT * FROM eventMaker.Column';
+  const sql = 'SELECT * FROM eventmaker.Column';
   connection.query(sql, (error, rows) => {
     if (error) {
       console.log('DB 접근 실패!');
       throw error;
     }
     res.status(200).json({
+      rows,
+    });
+  });
+});
+
+router.post('/add', (req, res) => {
+  const { title, pos } = req.body;
+  const params = [title, pos];
+  const sql = 'INSERT INTO eventmaker.Column(title, pos) VALUES(?, ?)';
+  connection.query(sql, params, (err, rows) => {
+    if (err) console.log(err);
+    res.json({
+      rows,
+    });
+  });
+});
+
+router.post('/delete', (req, res) => {
+  const { columnId } = req.body;
+  const sql = `DELETE FROM eventmaker.Column WHERE column_id = ${columnId}`;
+  connection.query(sql, (err, result) => {
+    if (err) console.log(err);
+    res.json({
+      result,
+    });
+  });
+});
+
+router.post('/update/title', (req, res) => {
+  const { columnId, title } = req.body;
+  const params = [title, columnId];
+  const sql = 'UPDATE eventmaker.Column SET title= ? WHERE column_id=?';
+  connection.query(sql, params, (err, rows) => {
+    if (err) console.log(err);
+    res.json({
+      rows,
+    });
+  });
+});
+
+router.post('/update/pos', (req, res) => {
+  const { columnId, pos } = req.body;
+  const params = [pos, columnId];
+  const sql = 'UPDATE eventmaker.Column SET pos= ? WHERE column_id=?';
+  connection.query(sql, params, (err, rows) => {
+    if (err) console.log(err);
+    res.json({
       rows,
     });
   });
