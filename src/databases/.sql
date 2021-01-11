112e807ca00e111f2461bcf8f9b5f3ef904c8f0b
DESKTOP-J5O26G9\admin <glesmallow@gmail.com>
2021-01-11 22:03:54 +0900

    [refactor] sql 리팩토링 및 수정
    DB의 테이블과 이름을 맞추기 위해 수정 및 잘못된 부분도 수정

--- src/databases/.sql
+++ src/databases/.sql
@@ -1,14 +1,16 @@
+-- Column 테이블
 create table eventmaker.Column(
-    column_id Integer PRIMARY KEY,
+    column_id Integer AUTO_INCREMENT PRIMARY KEY,
     title char(30) not null,
     pos Integer not null,
     create_At DATETIME DEFAULT NOW()
 );
 
+-- Card 테이블
 create table eventmaker.Card(
-  card_id Integer not null,
+  card_id Integer AUTO_INCREMENT not null,
   column_id Integer not null,
-  title Integer not null,
+  title char(30) not null,
   pos Integer not null,
   content text not null,
   author char(30) not null,
@@ -16,3 +18,15 @@ create table eventmaker.Card(
   PRIMARY KEY(card_id),
   FOREIGN KEY(column_id) REFERENCES eventmaker.Column(column_id)
 );
+
+-- 현재 Database들의 character 상태
+SELECT schema_name , default_character_set_name FROM information_schema.schemata ;
+
+-- Database의 character 상태를 변경
+ALTER DATABASE database_name DEFAULT CHARACTER SET utf8 ;
+
+-- 컬럼 character utf8로 변경
+ALTER TABLE eventmaker.Card CONVERT TO character SET utf8;
+
+-- 컬럼 변경 (예시 : title)
+ALTER TABLE eventmaker.Card MODIFY title char(30) NOT NULL;
