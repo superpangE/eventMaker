a744b97c404b522942ed9eb3a86534c889135875
superpangE <cdpkct@gmail.com>
2021-01-14 22:27:35 +0900

    [feat]Column 생성 컴포넌트 작성

---
+++ src/public/component/AddColumn/index.js
@@ -0,0 +1,8 @@
+import { getContainer } from '../common.js';
+
+const AddColumn = () => {
+  const AddContainer = getContainer(null, 'add-container', 'Add column');
+
+  return AddContainer;
+};
+export default AddColumn;
