e1326bf9b71efdb386fcb008a9c1c16b3b4cc37d
superpangE <cdpkct@gmail.com>
2021-01-16 14:48:48 +0900

    [feat]header 컴포넌트 추가 및 메인파일에 적용

--- src/public/component/index.js
+++ src/public/component/index.js
@@ -1,5 +1,6 @@
 import Column from './Column/index.js';
 import AddColumn from './AddColumn/index.js';
+import Header from './Header/index.js';
 import { getContainer } from './common.js';
 
 const AddColumnOnClickEvent = (MainContainer, AddColumnEle) => {
@@ -27,10 +28,14 @@ const AddColumnOnClickEvent = (MainContainer, AddColumnEle) => {
 
 const Main = () => {
   const MainContainer = getContainer(null, 'main-container', null);
+  const BodyContainer = getContainer(null, 'body-container', null);
   const AddColumnEle = AddColumn();
 
-  AddColumnOnClickEvent(MainContainer, AddColumnEle);
-  MainContainer.insertAdjacentElement('beforeend', AddColumnEle);
+  AddColumnOnClickEvent(BodyContainer, AddColumnEle);
+
+  BodyContainer.insertAdjacentElement('beforeend', AddColumnEle);
+  MainContainer.insertAdjacentElement('beforeend', Header());
+  MainContainer.insertAdjacentElement('beforeend', BodyContainer);
 
   return MainContainer;
 };
